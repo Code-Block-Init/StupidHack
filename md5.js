91c940d676b9c08e532a9b0e7227452df2d134bf
Aswini S <ashumeow@live.com>
2015-02-06 03:24:38 +0530

    Update md5.js

--- md5.js
+++ md5.js
@@ -1,4 +1,4 @@
-function md5(x, k) {
+function md5x(x, k) {
 var a = x[0], b = x[1], c = x[2], d = x[3];
 
 a = ff(a, b, c, d, k[0], 7, -680876936);
@@ -102,7 +102,7 @@ txt = '';
 var n = s.length,
 state = [1732584193, -271733879, -1732584194, 271733878], i;
 for (i=64; i<=s.length; i+=64) {
-md5(state, md5blk(s.substring(i-64, i)));
+md5x(state, md5blk(s.substring(i-64, i)));
 }
 s = s.substring(i-64);
 var tail = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
@@ -110,11 +110,11 @@ for (i=0; i<s.length; i++)
 tail[i>>2] |= s.charCodeAt(i) << ((i%4) << 3);
 tail[i>>2] |= 0x80 << ((i%4) << 3);
 if (i > 55) {
-md5(state, tail);
+md5x(state, tail);
 for (i=0; i<16; i++) tail[i] = 0;
 }
 tail[14] = n*8;
-md5(state, tail);
+md5x(state, tail);
 return state;
 }
 
